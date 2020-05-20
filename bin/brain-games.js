a11cdaa95a53f01087ed732c2d18388291b268b8
Yuriy Shulga <jsshu1ga@gmail.com>
2020-05-21 00:21:18 +0300

    the Eslint is configured

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-import { askNameUser } from '../src/cli.js'
-//import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!');
+import askNameUser from '../src/cli.js';
 
 askNameUser();
