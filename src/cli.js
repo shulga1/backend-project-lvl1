fb06218a6fd8fbafb5b4a9a128440ec497382011
Yuriy Shulga <jsshu1ga@gmail.com>
2020-05-29 21:00:35 +0300

    added default value

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,9 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!');
 
-  const nameUser = readlineSync.question('May I have your name? ');
+  const nameUser = readlineSync.question('May I have your name? ', {
+    defaultInput: 'Anonymous',
+  });
 
   console.log(`Hello, ${nameUser}!`);
 
