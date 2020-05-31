a354005ebfd0339d8310ecb1c81149cdfa9508f5
Yuriy Shulga <jsshu1ga@gmail.com>
2020-05-31 13:48:59 +0300

    added the game brain-gcd

---
+++ games/gcd.js
@@ -0,0 +1,31 @@
+import getRandomNumber from '../src/utilits.js';
+import engineGame from '../src/index.js';
+
+const getGreatestCommonDivisor = (firstNum, secondNum) => {
+  const minNum = Math.min(firstNum, secondNum);
+  const maxNum = Math.max(firstNum, secondNum);
+
+  if (maxNum % minNum === 0) {
+    return minNum;
+  }
+  return getGreatestCommonDivisor(maxNum % minNum, minNum);
+};
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const gsd = () => {
+  const firstNum = getRandomNumber(1000);
+  const secondNum = getRandomNumber(1000);
+  const question = `${firstNum} ${secondNum}`;
+  const correctAnswer = String(getGreatestCommonDivisor(firstNum, secondNum));
+
+  if (correctAnswer === '1') {
+    return gsd();
+  }
+
+  return { question, correctAnswer };
+};
+
+const game = () => engineGame(description, gsd);
+
+export default game;
