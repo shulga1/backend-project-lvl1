aa3180cf0e6de030ab9a961698522882f955e557
Yuriy Shulga <jsshu1ga@gmail.com>
2020-06-01 17:46:16 +0300

    added the game brain-prime

---
+++ games/prime.js
@@ -0,0 +1,33 @@
+import getRandomNumber from '../src/utilits.js';
+import engineGame from '../src/index.js';
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return 'no';
+  }
+
+  const iter = (div) => {
+    if (div === num) {
+      return 'yes';
+    }
+    if (num % div === 0) {
+      return 'no';
+    }
+
+    return iter(div + 1);
+  };
+
+  return iter(2);
+};
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const progression = () => {
+  const question = getRandomNumber();
+  const correctAnswer = isPrime(question);
+  return { question, correctAnswer };
+};
+
+const game = () => engineGame(description, progression);
+
+export default game;
